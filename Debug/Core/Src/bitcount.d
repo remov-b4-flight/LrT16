009565d1905260194e68e746a3e6fb301acdafee
remov-b4-flight <1328947+remov-b4-flight@users.noreply.github.com>
2026-06-07 17:31:32 +0900

    remove unneeded

--- Debug/Core/Src/bitcount.d
+++
@@ -1,2 +0,0 @@
-Core/Src/bitcount.o: ../Core/Src/bitcount.c ../Core/Inc/bitcount.h
-../Core/Inc/bitcount.h:
--- Debug/Core/Src/queue.d
+++
@@ -1,2 +0,0 @@
-Core/Src/queue.o: ../Core/Src/queue.c ../Core/Inc/queue.h
-../Core/Inc/queue.h:
